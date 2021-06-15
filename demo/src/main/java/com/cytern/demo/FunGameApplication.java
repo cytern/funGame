93646e83530207d263bdca58b02b45be08cbf0d2
cytern <2477185748@qq.com>
2021-06-15 13:38:11 +0800

    初次建立项目

---
+++ demo/src/main/java/com/cytern/demo/FunGameApplication.java
@@ -0,0 +1,13 @@
+package com.cytern.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunGameApplication.class, args);
+    }
+
+}
---
+++ demo/src/test/java/com/cytern/demo/FunGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cytern.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
